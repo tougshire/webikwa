d2aec699202f9a25bd17c540fa74659359c0a2e2
tougshire <tougshire@tougshire.com>
2025-06-05 06:37:15 -0400

    Update help text for save_page

--- management/commands/save_page.py
+++ management/commands/save_page.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from webikwa.models import IcalendarPage
 
 class Command(BaseCommand):
-    help = 'Auto saves pages - this is useful for pages like Icalendar page which update their data upon save'
+    help = 'Auto saves pages - this is useful for pages like Icalendar pages. ex: \'python manage.py save_page 86 47\' saves pages with ids 86 and 47'
 
     def add_arguments(self, parser):
         parser.add_argument('page_ids', nargs='+', type=int)
@@ -14,6 +14,6 @@ class Command(BaseCommand):
             except page.DoesNotExist:
                 raise CommandError('Page "%s" does not exist' % page_id)
 
-            page.save() #this should automatically update the calendar
+            page.save() #Icalendar pages will rewrite their data upon save
 
             self.stdout.write(self.style.SUCCESS('Successfully upated page "%s"' % page_id))
